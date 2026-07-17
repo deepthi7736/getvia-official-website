8fbadc097b23facb81af75b4f0a0ed9292d57470
deepthi7736 <deepthimolharidas@gmail.com>
2026-07-17 16:58:30 +0530

    Create ScrollProgress.jsx

---
+++ src/components/ScrollProgress.jsx
@@ -0,0 +1,18 @@
+import { motion, useScroll, useSpring } from "framer-motion";
+
+export default function ScrollProgress() {
+  const { scrollYProgress } = useScroll();
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 120,
+    damping: 28,
+    restDelta: 0.001,
+  });
+
+  return (
+    <motion.div
+      style={{ scaleX }}
+      className="fixed left-0 right-0 top-0 z-[100] h-1 origin-left bg-[#8CE72A]"
+    />
+  );
+}
