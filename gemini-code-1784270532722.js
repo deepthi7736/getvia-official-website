e006f127ad00db2c9794c0bf47c0d207489bc02d
deepthi7736 <deepthimolharidas@gmail.com>
2026-07-17 12:12:42 +0530

    Add files via upload

---
+++ gemini-code-1784270532722.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Manrope', 'sans-serif'],
+        serif: ['Fraunces', 'serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
