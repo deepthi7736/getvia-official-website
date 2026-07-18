452fe7f41c2aa94eb9a7ac3874a08ffb99964190
deepthi7736 <deepthimolharidas@gmail.com>
2026-07-18 15:46:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { ArrowUpRight, Menu, X } from "lucide-react";
-import logo from "../assets/getvia-full-logo.png";
+import logo from "../assets/getvia-logo.png";
 
 const LINKS = [
   { href: "#about", label: "About" },
