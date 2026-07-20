71f2e36e64cff37e3d79611b3be1ac89b98093d1
deepthi7736 <deepthimolharidas@gmail.com>
2026-07-20 16:17:35 +0530

    Add files via upload

---
+++ src/components/App (3).jsx
@@ -0,0 +1,37 @@
+import ScrollProgress from "./components/ScrollProgress";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import WhatWeDo from "./components/WhatWeDo";
+import Features from "./components/Features";
+import Ecosystem from "./components/Ecosystem";
+import PlatformBusiness from "./components/PlatformBusiness";
+import Technology from "./components/Technology";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <ScrollProgress />
+      <Navbar />
+
+      <main>
+        <Hero />
+        <About />
+        <WhatWeDo />
+        <Features />
+        <Ecosystem />
+        <PlatformBusiness />
+        <Technology />
+        <Testimonials />
+        <Contact />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
