dd32cf10ff75cc18b996bcdbb28e5e69e5895c29
deepthi7736 <deepthimolharidas@gmail.com>
2026-07-21 11:53:32 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,27 @@
+import Hero from "../components/Hero";
+import About from "../components/About";
+import WhatWeDo from "../components/WhatWeDo";
+import Features from "../components/Features";
+import Ecosystem from "../components/Ecosystem";
+import PlatformBusiness from "../components/PlatformBusiness";
+import Technology from "../components/Technology";
+import Testimonials from "../components/Testimonials";
+import FAQ from "../components/FAQ";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <About />
+      <WhatWeDo />
+      <Features />
+      <Ecosystem />
+      <PlatformBusiness />
+      <Technology />
+      <Testimonials />
+      <FAQ />
+      <Contact />
+    </main>
+  );
+}
